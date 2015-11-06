c02a926346c9edd3b44ec385fab834db20b1e576
bwalsh712 <bwalsh712@gmail.com>
2015-11-06 13:57:21 -0800

    login/register almost working

--- public/javascript/controllers/GlobalController.js
+++ public/javascript/controllers/GlobalController.js
@@ -9,7 +9,23 @@
     vm.user = {};
     vm.status = UserFactory.status;
 
+	vm.logout = function() {
+	UserFactory.logout();
+	$state.go('Home');
+};
 
+vm.registerUser = function() {
+	UserFactory.registerUser(vm.user).then(function() {
+		$state.go('Home');
+	});
+};
+
+vm.loginUser = function() {
+	UserFactory.loginUser(vm.user).then(function() {
+		$state.go('Home');
+		vm.user = {};
+	});
+};
 
 
   }
