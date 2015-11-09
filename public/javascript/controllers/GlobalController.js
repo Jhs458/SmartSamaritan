1fb439dbeae6ae1399a5927fa33081bf924f8143
LaurenLamborne <laurenlouann@gmail.com>
2015-11-09 13:05:13 -0500

    styled splash login form and changed registration process

--- public/javascript/controllers/GlobalController.js
+++ public/javascript/controllers/GlobalController.js
@@ -6,51 +6,50 @@
 	function GlobalController(UserFactory, $mdSidenav, $state, JobsFactory) {
 		var vm = this;
 		vm.isLogin = true;
-    vm.user = {};
-    vm.status = UserFactory.status;
+		vm.user = {};
+		vm.status = UserFactory.status;
 		vm.msg = {};
 
-
-	vm.logout = function() {
-		UserFactory.logout();
-		$state.go('Home');
+		
+		vm.logout = function() {
+			UserFactory.logout();
+			$state.go('Splash');
 		};
 
-	vm.close = function () {
-			$mdSidenav('right').toggle();
-			    };
+		vm.registerUser = function() {
+			UserFactory.registerUser(vm.user).then(function() {
+				$state.go('Dashboard');
+			});
+		};
 
-	vm.toggleRight = function () {
-			$mdSidenav('right').toggle();
-			    };
+		vm.loginUser = function() {
+			UserFactory.loginUser(vm.user).then(function() {
+				$state.go('Dashboard');
+				vm.user = {};
+				nav.user.username= null;
+				console.log(vm.status);
+			});
+		};
 
-	vm.sendMsg = function(){
-		JobsFactory.sendMsg(vm.msg).then(function(res) {
+		vm.close = function () {
+			$mdSidenav('right').toggle();
+		};
+
+		vm.toggleRight = function () {
+			$mdSidenav('right').toggle();
+		};
+
+		vm.sendMsg = function(){
+			JobsFactory.sendMsg(vm.msg).then(function(res) {
 				// vm.close();
 				vm.msg = {};
-					});
-				};
-
-	JobsFactory.getAllMessages().then(function(res) {
-				vm.messages = res;
-			});
-
-
-	vm.registerUser = function() {
-		UserFactory.registerUser(vm.user).then(function() {
-			$state.go('Home');
 			});
 		};
 
-	vm.loginUser = function() {
-		UserFactory.loginUser(vm.user).then(function() {
-			$state.go('Services');
-			vm.user = {};
-			console.log(vm.status);
-			});
-		};
+		JobsFactory.getAllMessages().then(function(res) {
+			vm.messages = res;
+		});
 
 
-
-  }
+	}
 })();
